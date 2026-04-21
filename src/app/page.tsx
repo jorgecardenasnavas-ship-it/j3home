c17b98420a74cb1e221d423af6d597f328c31590
Jorge <JCodesMore@users.noreply.github.com>
2026-04-21 23:55:43 +0200

    feat: wire CatalogIntro and FooterClose into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Navbar } from "@/components/Navbar";
 import { HeroSection } from "@/components/HeroSection";
 import { SponsorsBanner } from "@/components/SponsorsBanner";
+import { CatalogIntro } from "@/components/CatalogIntro";
 import { ProductsGrid } from "@/components/ProductsGrid";
+import { FooterClose } from "@/components/FooterClose";
 import { Footer } from "@/components/Footer";
 
 export default function Home() {
@@ -11,7 +13,9 @@ export default function Home() {
       <main>
         <HeroSection />
         <SponsorsBanner />
+        <CatalogIntro />
         <ProductsGrid />
+        <FooterClose />
       </main>
       <Footer />
     </>
